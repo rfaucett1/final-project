0de862c366571926c4340047eddf5408b127fa24
rfaucett1 <ruth.faucett@bsu.edu>
2025-12-06 11:18:24 -0500

    Add files via upload

---
+++ ship.py
@@ -0,0 +1,29 @@
+import pygame, simpleGE
+
+class Ship(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("ship.png")
+        self.position =(320, 400)
+        self.moveSpeed = 5
+        
+    def process(self):
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x-=self.moveSpeed
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.moveSpeed
+        if self.isKeyPressed(pygame.K_UP):
+            self.y -=self.moveSpeed
+        if self.isKeyPressed(pygame.K_DOWN):
+            self.y +=self.moveSpeed
+                 
+             
+def main():
+    game = simpleGE.Scene()
+    game.setCaption("Pygame in SPAAAAACE!")
+    ship = Ship(game)
+    game.sprites = [ship]
+    game.start()
+    
+if __name__ == "__main__":
+    main()
